4adaf180c9dab61b8a7a54579340554265b548ea
Ngayep <courtesyofjess@gmail.com>
2024-08-03 18:01:19 +0100

    pythonroute

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """script that starts a Flask web application
-with different routes
-"""
+with different routesand at a particular port"""
 
 from flask import Flask
 app = Flask("__name__")
