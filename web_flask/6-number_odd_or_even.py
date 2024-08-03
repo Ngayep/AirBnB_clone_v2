d99833420941c18b18a9e3d0bc434a06c2063175
Ngayep <courtesyofjess@gmail.com>
2024-08-03 19:14:13 +0100

    odd-even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -32,16 +32,21 @@ def pythonText(text="is cool"):
 @app.route('/number/<int:n>', strict_slashes=False)
 def isNumber(n):
     """display “n is a number” only if n is an integer"""
-    if isinstance(n, int):
-        return "{} is a number".format(n)
+    return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n=None):
     """display a HTML page only if n is an integer"""
-    if isinstance(n, int):
-        return render_template("5-number.html", n=n)
+    return render_template("5-number.html", n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """Display a HTML page only if n is an integer, indicating if it's even or odd"""
+    parity = "even" if n % 2 == 0 else "odd"
+    return render_template("6-number_odd_or_even.html", n=n, parity=parity)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
