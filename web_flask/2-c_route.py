a51673d0e8e070d9cd3479f82e801e469fbcdc87
Ngayep <courtesyofjess@gmail.com>
2024-08-03 17:52:35 +0100

    pythonroute

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" a script that starts a Flask web application
+with 3 routes
+"""
+
+
+from flask import Flask
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return a certain string"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cText(text):
+    """return "C" with some value"""
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
